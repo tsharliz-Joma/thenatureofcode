40fac92afc22f7a41c792cf279f77eaa2824e6f5
Charles Joma <almprincecharles@yahoo.com>
2025-10-23 21:02:43 +1100

    Background :: Add a horizon, grass and sun to the canvas

--- chptOneProject/js/sketch.js
+++ chptOneProject/js/sketch.js
@@ -1,23 +1,43 @@
 let beeImage;
-let randomCounts = [];
 let beeWalker;
+let sunHeight;
+let horizon = 500;
 
 function preload() {
   beeImage = loadImage("images/beeavatar.png");
 }
+
 function setup() {
   background(220);
   createCanvas(windowWidth, windowHeight);
-  beeImage.resize(50, 50)
+  beeImage.resize(50, 50);
   beeWalker = new BeeWalker(beeImage, 0, 10000, 50, 50);
-
 }
 
 function draw() {
-  // this background called here clears the frame so you dont see the old instances of the bee walker
   background(220);
+
+  // Sun follows the X-coordinate of mouse
+  sunHeight = mouseX;
+  if (sunHeight < horizon) {
+    background("lightblue");
+  } else {
+    background("rgb(21,0,82)");
+  }
+  // this background called here clears the frame so you dont see the old instances of the bee walker
+
+  // Sun
+  fill("yellow");
+  noStroke();
+  circle(windowWidth / 4, sunHeight, 100);
+
+  // Horizon
+  stroke("green");
+  line(0, horizon, windowWidth, horizon);
+
+  // Grass
+  fill('green')
+  rect(0, horizon, windowWidth, horizon)
   beeWalker.show();
-  beeWalker.walk()
-
+  beeWalker.walk();
 }
-
