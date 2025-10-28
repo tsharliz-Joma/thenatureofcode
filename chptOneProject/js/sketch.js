9a8357bce48adc53126ff70ffa373b3c483fcd44
Charles Joma <almprincecharles@yahoo.com>
2025-10-28 16:33:19 +1100

    Chapter one complete

--- chptOneProject/js/sketch.js
+++ chptOneProject/js/sketch.js
@@ -1,22 +1,28 @@
 let beeImage;
+let treeImage;
 let beeWalker;
 let sunHeight;
 let horizon = 500;
+let acc = 0.0;
+let perlinGrass;
 
 function preload() {
   beeImage = loadImage("images/beeavatar.png");
+  treeImage = loadImage("images/tree.png");
 }
 
 function setup() {
-  background(220);
   createCanvas(windowWidth, windowHeight);
+  background(220);
+  pixelDensity(1);
   beeImage.resize(50, 50);
+  treeImage.resize(400, 400);
   beeWalker = new BeeWalker(beeImage, 0, 10000, 50, 50);
+  perlinGrass = new PerlinGrass(0, horizon, width, height - horizon, 0.01);
 }
 
 function draw() {
   background(220);
-
   // Sun follows the X-coordinate of mouse
   sunHeight = mouseX;
   if (sunHeight < horizon) {
@@ -24,11 +30,9 @@ function draw() {
   } else {
     background("rgb(21,0,82)");
   }
-  // this background called here clears the frame so you dont see the old instances of the bee walker
-
   // Sun
-  fill("yellow");
   noStroke();
+  fill("yellow");
   circle(windowWidth / 4, sunHeight, 100);
 
   // Horizon
@@ -36,8 +40,22 @@ function draw() {
   line(0, horizon, windowWidth, horizon);
 
   // Grass
-  fill('green')
-  rect(0, horizon, windowWidth, horizon)
+  perlinGrass.draw();
+  image(treeImage, 0, horizon - treeImage.height);
+
+  const topLx = 600;
+  const topLy = horizon;
+  const topRx = 800;
+  const topRy = horizon;
+  const bottomRx = 800;
+  const bottomRy = horizon + 600;
+  const bottomLx = 600;
+  const bottomLy = horizon + 600;
+  // River
+  fill("blue");
+  noStroke();
+  quad(topLx, topLy, topRx, topRy, bottomRx, bottomRy, bottomLx, bottomLy);
+
   beeWalker.show();
   beeWalker.walk();
 }
--- chptOneProject/js/walker.js
+++ chptOneProject/js/walker.js
@@ -19,3 +19,42 @@ class BeeWalker {
     this.ny += 0.01;
   }
 }
+
+class PerlinGrass {
+  constructor(x, y, w, h, acc) {
+    this.w = w;
+    this.h = h;
+    this.x = x;
+    this.y = y;
+    this.acc = acc;
+    this.c1 = color("#0ea5a0"); // teal-green
+    this.c2 = color("#16a34a");
+    this.buildTexture();
+  }
+
+  buildTexture() {
+    const w = max(0, this.w);
+    const h = max(0, this.h);
+    this.tex = createImage(w, h);
+    this.tex.loadPixels();
+
+    for (let j = 0; j < h; j++) {
+      const py = this.y + j;
+      for (let i = 0; i < w; i++) {
+        const px = this.x + i;
+        const n = noise(px * this.acc, py * this.acc);
+        const col = lerpColor(this.c1, this.c2, n);
+        const index = (j * w + i) * 4;
+        this.tex.pixels[index] = red(col);
+        this.tex.pixels[index + 1] = green(col);
+        this.tex.pixels[index + 2] = blue(col);
+        this.tex.pixels[index + 3] = 255;
+      }
+    }
+    this.tex.updatePixels();
+  }
+
+  draw() {
+    image(this.tex, this.x, this.y);
+  }
+}
